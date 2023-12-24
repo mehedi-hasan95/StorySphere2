5329aa52ff94adb7d896e2bbb682dbfd1ac7445d
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-12-24 09:06:23 +0600

    Make the unpublished post as client

--- src/app/admin/unpublished/page.tsx
+++ src/app/admin/unpublished/page.tsx
@@ -1,8 +1,9 @@
+"use client";
 import { Separator } from "@/components/ui/separator";
-import prismadb from "@/lib/prismadb";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import useSWR from "swr";
 
 interface UnPublishedPageParams {
   id: string;
@@ -10,20 +11,23 @@ interface UnPublishedPageParams {
   title: string;
 }
 
-const UnPublishedPage = async () => {
-  const data = await prismadb.posts.findMany({
-    where: {
-      verifide: false,
-    },
-  });
+export const fetchCache = "force-no-store";
+const UnPublishedPage = () => {
+  const { data, isLoading } = useSWR(
+    `${process.env.NEXT_API_URL}/admin/unpublished`
+  );
+  if (isLoading) return <div>loading...</div>;
+  if (data?.status === 401) {
+    <p>Unauthorize user</p>;
+  }
   return (
     <div>
       <h2 className="md:text-xl font-bold pt-5">
-        Unverified Posts ({data?.length})
+        Unverified Posts ({data?.posts?.length})
       </h2>
       <Separator className={cn("my-5")} />
       <div className="grid md:grid-cols-2 gap-5">
-        {data?.map((item: UnPublishedPageParams) => (
+        {data?.posts?.map((item: UnPublishedPageParams) => (
           <Link href={`/admin/unpublished/${item.id}`} key={item.id}>
             <Image
               src={item.image}
