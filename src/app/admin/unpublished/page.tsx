e97c1d815b1cf1f35ab23352801969b73bbdffef
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-12-25 20:40:46 +0600

    Add revalidate

--- src/app/admin/unpublished/page.tsx
+++ src/app/admin/unpublished/page.tsx
@@ -1,10 +1,11 @@
+"use client";
+export const fetchCache = "force-no-store";
 export const revalidate = 0;
-import getCurrentUser from "@/actions/getCurrentUser";
 import { Separator } from "@/components/ui/separator";
-import prismadb from "@/lib/prismadb";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import useSWR from "swr";
 
 interface UnPublishedPageParams {
   id: string;
@@ -12,34 +13,34 @@ interface UnPublishedPageParams {
   title: string;
 }
 
-const UnPublishedPage = async () => {
-  const currentUser = await getCurrentUser();
-  const data = await prismadb.posts.findMany({
-    where: {
-      verifide: false,
-      userId: currentUser?.id,
-    },
-  });
+const UnPublishedPage = () => {
+  const { data, isLoading } = useSWR(`/api/admin/unpublished`);
+  if (isLoading) return <div>loading...</div>;
+  if (data?.status === 401) {
+    <p>Unauthorize user</p>;
+  }
   return (
     <div>
       <h2 className="md:text-xl font-bold pt-5">
-        Unverified Posts ({data?.length})
+        Unverified Posts ({data?.posts?.length})
       </h2>
       <Separator className={cn("my-5")} />
-      <div className="grid md:grid-cols-2 gap-5">
-        {data?.map((item: UnPublishedPageParams) => (
-          <Link href={`/admin/unpublished/${item.id}`} key={item.id}>
-            <Image
-              src={item.image}
-              alt=""
-              height={500}
-              width={500}
-              className=""
-            />
-            <h2 className="md:text-lg font-bold">{item.title}</h2>
-          </Link>
-        ))}
-      </div>
+      {data?.posts?.length > 0 && (
+        <div className="grid md:grid-cols-2 gap-5">
+          {data?.posts?.map((item: UnPublishedPageParams) => (
+            <Link href={`/admin/unpublished/${item.id}`} key={item.id}>
+              <Image
+                src={item.image}
+                alt=""
+                height={500}
+                width={500}
+                className=""
+              />
+              <h2 className="md:text-lg font-bold">{item.title}</h2>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
